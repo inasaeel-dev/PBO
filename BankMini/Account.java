43d09783f7760f18d3abed3ef37c4607754149cb
Inas Asami El <inasasami11@gmail.com>
2026-09-05 20:58:53 +0700

    langkah 1

---
+++ BankMini/Account.java
@@ -0,0 +1,19 @@
+package BankMini;
+
+public class Account {
+  public String ownerName;
+  public double balance;
+
+  }
+
+  // public Account(String ownerName, double balance){
+  //   this.ownerName = ownerName;
+  //   this.balance = balance;
+  // }
+
+  // public String formatBalance() {
+  //   return String.format("%, .2f", balance);
+  // }
+
+  // public boolean isOverDrawn() {
+  //   return balance < 0;
